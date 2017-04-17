bb95cc08bd560a756d1e41d99a925022a75642f0
Hartaarn <hartaarn@gmail.com>
2017-04-17 00:16:11 -0400

    Implemented bonus Updated styling

--- module-5-solution/src/public/sign-up/sign-up.controller.js
+++ module-5-solution/src/public/sign-up/sign-up.controller.js
@@ -2,7 +2,27 @@
 "use strict";
 
 angular.module('public')
-.controller('SignUpController', SignUpController);
+.controller('SignUpController', SignUpController)
+.directive('itemExists', ['MenuService', '$filter', function(MenuService, $filter) {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attr, ctrl) {
+            function itemValidation(value) {
+            	console.log('in itemValidation');
+                MenuService.getMenuItem($filter('uppercase')(value)).then(function(){
+                	ctrl.$setValidity('validItem', true);
+                }).catch(function(){
+                	ctrl.$setValidity('validItem', false);
+                })
+
+                return value;
+            }
+            ctrl.$parsers.push(itemValidation);
+        }
+    };
+}]);
+
+
 
 SignUpController.$inject = ['UserService','MenuService','$filter'];
 function SignUpController(UserService, MenuService, $filter) {
@@ -27,28 +47,30 @@ function SignUpController(UserService, MenuService, $filter) {
   	  promise.then(function (response) {
       	//$ctrl.favItem = response;
       	$ctrl.searchInvalid = false;
+      	//$ctrl.$setValidity('validItem', true);
       	UserService.setFavItem(response);
       	UserService.setUser($ctrl.user);
-      	UserService.setFavItemShortName( $filter('uppercase')($ctrl.favItemShortName) );
+      	//UserService.setFavItemShortName( $filter('uppercase')($ctrl.favItemShortName) );
       	$ctrl.signUpSuccess = true;
       	return true;
      })
      .catch(function (error) {
      	$ctrl.searchInvalid = true;
+     	//$ctrl.$setValidity('validItem', false);
        	return false;
      });
   }
 
-  $ctrl.validateFavItem = function() {
-      var promise = MenuService.getMenuItem( $filter('uppercase')($ctrl.favItemShortName) );
+  // $ctrl.validateFavItem = function() {
+  //     var promise = MenuService.getMenuItem( $filter('uppercase')($ctrl.favItemShortName) );
 
-      promise.then(function () {
-          $ctrl.searchInvalid = false;
-        })
-        .catch(function() {
-          $ctrl.searchInvalid = true;
-        });
-    }
+  //     promise.then(function () {
+  //         $ctrl.searchInvalid = false;
+  //       })
+  //       .catch(function() {
+  //         $ctrl.searchInvalid = true;
+  //       });
+  //   }
 
 }
 
